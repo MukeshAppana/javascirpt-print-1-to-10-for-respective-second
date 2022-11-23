7df3586109e5fc4d24aa28812d09b9029c3b9caa
MukeshAppana <109844172+MukeshAppana@users.noreply.github.com>
2022-11-23 12:58:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//with using let
+//with using let (Type1)
 function x() {
     for (let i=1;i<=10;++i){
         setTimeout(function(){
@@ -9,17 +9,17 @@ function x() {
 
 x();
 
-//with using var 
+//with using var (Type2)
 
-// function x() {
-//     for (var i=1;i<=10;++i){
-//         function close(i){
-//             setTimeout(function(){
-//                 console.log(i);
-//             },i*1000);
-//         }
-//         close(i);
-//     }
-// }
+ function x() {
+     for (var i=1;i<=10;++i){
+         function close(i){
+             setTimeout(function(){
+                 console.log(i);
+             },i*1000);
+         }
+         close(i);
+     }
+ }
 
-// x();
+ x();
